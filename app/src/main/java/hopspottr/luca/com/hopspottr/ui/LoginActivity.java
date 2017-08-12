bff8dce5a46a945c82c1c186b308c095b28d2a13
kazimir-willems <kazimirwillems@outlook.com>
2017-08-12 04:32:23 +0200

    - Login Activity (Without Facebook Button)

--- app/src/main/java/hopspottr/luca/com/hopspottr/ui/LoginActivity.java
+++ app/src/main/java/hopspottr/luca/com/hopspottr/ui/LoginActivity.java
@@ -1,13 +1,15 @@
-package hopspottr.luca.com.hopspottr;
+package hopspottr.luca.com.hopspottr.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import hopspottr.luca.com.hopspottr.R;
+
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
     }
 }

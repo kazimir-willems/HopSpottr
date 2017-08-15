7d6d2a15c559e6ec7517e0c08bd57772803143f4
kazimir-willems <kazimirwillems@outlook.com>
2017-08-15 18:35:38 +0100

    - Added Tab, Fragments, Navigation Menu

--- app/src/main/java/hopspottr/luca/com/hopspottr/ui/HomeActivity.java
+++ app/src/main/java/hopspottr/luca/com/hopspottr/ui/HomeActivity.java
@@ -13,11 +13,18 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.nostra13.universalimageloader.core.ImageLoader;
 
+import de.hdodenhof.circleimageview.CircleImageView;
 import hopspottr.luca.com.hopspottr.R;
+import hopspottr.luca.com.hopspottr.fragment.ActivityFragment;
+import hopspottr.luca.com.hopspottr.fragment.FavoriteFragment;
+import hopspottr.luca.com.hopspottr.fragment.NewsFragment;
+import hopspottr.luca.com.hopspottr.fragment.SearchFragment;
+import hopspottr.luca.com.hopspottr.fragment.TrendingFragment;
 import hopspottr.luca.com.hopspottr.util.SharedPrefManager;
 
 public class HomeActivity extends AppCompatActivity
@@ -26,12 +33,38 @@ public class HomeActivity extends AppCompatActivity
     private TextView tvFullName;
     private ImageView ivPhoto;
 
+    private ImageView ivSearch;
+    private ImageView ivNews;
+    private ImageView ivTrending;
+    private ImageView ivFavorite;
+    private ImageView ivActivity;
+
+    private LinearLayout searchLayout;
+    private LinearLayout newsLayout;
+    private LinearLayout trendingLayout;
+    private LinearLayout favoriteLayout;
+    private LinearLayout activityLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        ivSearch = (ImageView) findViewById(R.id.iv_search);
+        ivNews = (ImageView) findViewById(R.id.iv_news);
+        ivTrending = (ImageView) findViewById(R.id.iv_trending);
+        ivFavorite = (ImageView) findViewById(R.id.iv_favorite);
+        ivActivity = (ImageView) findViewById(R.id.iv_activity);
+
+        searchLayout = (LinearLayout) findViewById(R.id.search_layout);
+        newsLayout = (LinearLayout) findViewById(R.id.news_layout);
+        trendingLayout = (LinearLayout) findViewById(R.id.trending_layout);
+        favoriteLayout = (LinearLayout) findViewById(R.id.favorite_layout);
+        activityLayout = (LinearLayout) findViewById(R.id.activity_layout);
+
+        initUI();
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -42,6 +75,8 @@ public class HomeActivity extends AppCompatActivity
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
+        navigationView.setItemIconTintList(null);
+
         View header = navigationView.getHeaderView(0);
 
         tvFullName = (TextView) header.findViewById(R.id.tv_name);
@@ -49,6 +84,85 @@ public class HomeActivity extends AppCompatActivity
 
         tvFullName.setText(SharedPrefManager.getInstance(this).getFullName());
         ImageLoader.getInstance().displayImage(SharedPrefManager.getInstance(this).getAvatarUrl(), ivPhoto);
+
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.content_fragment, SearchFragment.newInstance("", ""))
+                .commit();
+    }
+
+    private void initUI() {
+        searchLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setDefaultIcon();
+                ivSearch.setBackground(getResources().getDrawable(R.drawable.ic_search_selected));
+
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.content_fragment, SearchFragment.newInstance("", ""))
+                        .commit();
+            }
+        });
+
+        newsLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setDefaultIcon();
+                ivNews.setBackground(getResources().getDrawable(R.drawable.ic_news_selected));
+
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.content_fragment, NewsFragment.newInstance("", ""))
+                        .commit();
+                getSupportActionBar().setTitle(R.string.title_news_feed);
+            }
+        });
+
+        trendingLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setDefaultIcon();
+                ivTrending.setBackground(getResources().getDrawable(R.drawable.ic_trending_selected));
+
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.content_fragment, TrendingFragment.newInstance("", ""))
+                        .commit();
+
+                getSupportActionBar().setTitle(R.string.title_trending);
+            }
+        });
+
+        favoriteLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setDefaultIcon();
+                ivFavorite.setBackground(getResources().getDrawable(R.drawable.ic_favorite_selected));
+
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.content_fragment, FavoriteFragment.newInstance("", ""))
+                        .commit();
+                getSupportActionBar().setTitle(R.string.title_favorites);
+            }
+        });
+
+        activityLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setDefaultIcon();
+                ivActivity.setBackground(getResources().getDrawable(R.drawable.ic_active_selected));
+
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.content_fragment, ActivityFragment.newInstance("", ""))
+                        .commit();
+                getSupportActionBar().setTitle(R.string.title_activity);
+            }
+        });
+    }
+
+    private void setDefaultIcon() {
+        ivSearch.setBackground(getResources().getDrawable(R.drawable.ic_search));
+        ivNews.setBackground(getResources().getDrawable(R.drawable.ic_news));
+        ivTrending.setBackground(getResources().getDrawable(R.drawable.ic_trending));
+        ivFavorite.setBackground(getResources().getDrawable(R.drawable.ic_favorite));
+        ivActivity.setBackground(getResources().getDrawable(R.drawable.ic_activity));
     }
 
     @Override
@@ -62,22 +176,14 @@ public class HomeActivity extends AppCompatActivity
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.home, menu);
-        return true;
-    }
-
-    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (id) {
+
         }
 
         return super.onOptionsItemSelected(item);
@@ -88,20 +194,6 @@ public class HomeActivity extends AppCompatActivity
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
-
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
-        }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
